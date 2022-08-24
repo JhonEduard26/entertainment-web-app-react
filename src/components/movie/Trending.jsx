88832ca365bba14e39e5a7948a44c05f54dfba39
Jhon Eduard Bocanegra <jhon.bocanegra26@gmail.com>
2022-08-23 21:05:35 -0500

    [UPDATE] colors

--- src/components/movie/Trending.jsx
+++ src/components/movie/Trending.jsx
@@ -1,33 +1,32 @@
-import { useSelector } from 'react-redux'
-import { Loader } from '../ui/Loader'
-import { MovieCard } from "./MovieCard"
+import { useSelector } from "react-redux";
+import { Loader } from "../ui/Loader";
+import { MovieCard } from "./MovieCard";
 
 export const Trending = () => {
-
-  const { movies } = useSelector(state => state.movies)
-  const { isLoading } = useSelector(state => state.ui)
+  const { movies } = useSelector((state) => state.movies);
+  const { isLoading } = useSelector((state) => state.ui);
 
   return (
     <section className="trending">
       <h2 className="trending__title">Trending</h2>
       <div className="trending__slider">
-        {
-          isLoading
-            ? <Loader />
-            : movies.map((movie) => (
-              <MovieCard
-                key={movie.id}
-                genres={movie.genre_ids}
-                id={movie.id}
-                image={movie.backdrop_path}
-                isAdult={movie.adult}
-                overview={movie.overview}
-                release={movie.release_date}
-                title={movie.original_title}
-              />
-            ))
-        }
+        {isLoading ? (
+          <Loader />
+        ) : (
+          movies.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              genres={movie.genre_ids}
+              id={movie.id}
+              image={movie.backdrop_path}
+              isAdult={movie.adult}
+              overview={movie.overview}
+              release={movie.release_date}
+              title={movie.original_title}
+            />
+          ))
+        )}
       </div>
     </section>
-  )
-}
+  );
+};
--- src/components/ui/Search.jsx
+++ src/components/ui/Search.jsx
@@ -1,21 +1,25 @@
-import { useNavigate } from 'react-router-dom'
-import search from '../../assets/icons/search.svg'
+import { useNavigate } from "react-router-dom";
+import search from "../../assets/icons/search.svg";
 
 export const Search = () => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleSearch = (e) => {
-    if (e.key === 'Enter') {
-      console.log('Enter key pressed')
-      navigate(`/search/${e.target.value}`)
+    if (e.key === "Enter") {
+      console.log("Enter key pressed");
+      navigate(`/search/${e.target.value}`);
     }
-  }
+  };
 
   return (
     <div className="search">
       <img src={search} alt="" />
-      <input className="search__input" type="text" placeholder="Search movies or tv series" onKeyDown={handleSearch} />
+      <input
+        className="search__input"
+        type="text"
+        placeholder="Search movies"
+        onKeyDown={handleSearch}
+      />
     </div>
-  )
-}
+  );
+};
